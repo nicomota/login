49f4d31c83ec7e59351750427d5c793b5f7d477b
Nicolas da Mota Monteiro dos Santos <nicolas.mota2000@example.com>
2023-05-15 13:25:52 -0300

    NovaAtualizacao

--- script.js
+++ script.js
@@ -1,11 +1,70 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.querySelector("form");
+let textForm = document.getElementById("textForm");
+let textEmail = document.getElementById("textEmail");
+let textPassword = document.getElementById("textPassword");
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+form.addEventListener("submit", (e) => {
+  if (email.value == "" && password.value == "") {
+    textForm.textContent = "Você precisa preencher todos os campos!";
+  } else if (
+    validatorEmail(email.value) === true &&
+    validatorPassword(password.value) === true
+  ) {
+    console.log(email.value);
+    console.log(password.value);
+    textForm.textContent = "";
+    textEmail.textContent = "";
+    textPassword.textContent = "";
+  } else {
+    console.log("Requisição não atendida");
+  }
+
+  e.preventDefault();
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+email.addEventListener("keyup", () => {
+  if (validatorEmail(email.value) !== true) {
+	textEmail.textContent = "O formato do email deve ser EX: name@jeaholding.com.br"
+} else {
+    textEmail.textContent = "";
+  }
 });
+
+password.addEventListener("keyup", () => {
+  if (validatorPassword(password.value) !== true) {
+    textPassword.textContent =
+	textPassword.textContent = "Necessário 1 caracter especial, 1 número e uma letra maiúscula.";
+} else {
+    textPassword.textContent = "";
+  }
+});
+
+function validatorEmail(email) {
+  let emailPattern =
+    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  return emailPattern.test(email);
+}
+
+function validatorPassword(password) {
+  let passwordPattern =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  return passwordPattern.test(password);
+}
+
+
+function logar(){
+
+	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+
+	if(email =="teste@gmail.com" && password == "@Teste123"){
+		alert('Login Realizado');
+		location.href = "holding/dash/index.html";
+	}else{
+		alert('Usuario ou senha incorretos');
+	}
+
+}
+
